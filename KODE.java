fef242bcd80221a4ab6ce389bc28bfb1efdbd915
Danish Zaidan <95460214+Dzkamil@users.noreply.github.com>
2024-06-03 14:55:42 +0700

    Update KODE.java

--- KODE.java
+++ KODE.java
@@ -15,3 +15,28 @@ public class uasSem2 {
             }
             cityAlliances = new HashMap<>();
         }
+        void addAlliance(int color, List<Integer> cities) {
+            for (int city : cities) {
+                cityAlliances.putIfAbsent(city, new HashSet<>());
+                cityAlliances.get(city).add(color);
+            }
+            for (int city : cities) {
+                for (int otherCity : cities) {
+                    if (city != otherCity) {
+                        adjList.get(city).add(otherCity);
+                    }
+                }
+            }
+        }
+
+        boolean canVisitAllCitiesFrom(int startCity) {
+            Set<Integer> initialTickets = cityAlliances.get(startCity);
+            for (int ticket : initialTickets) {
+                Set<Integer> visited = new HashSet<>();
+                if (dfs(startCity, visited, ticket) == numCities) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
