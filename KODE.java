7d6c6c24818738a54c404e845cf6f396bdfeba42
dewisiti28 <dewisitijamilah@upi.edu>
2024-06-08 19:48:36 +0700

    Memperbaiki eror KODE.java

--- KODE.java
+++ KODE.java
@@ -1,40 +1,34 @@
 import java.util.*;
+import java.util.LinkedList;
 
 public class UAS {
-
-   
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        StringBuilder output = new StringBuilder();
+        List<Integer> hasil = new ArrayList<>();
 
         while (true) {
             int jumlahKota = scanner.nextInt();
             int jumlahAliansi = scanner.nextInt();
-            if (jumlahKota == 0 && jumlahAliansi == 0) break;
+            if (jumlahKota == 0 && jumlahAliansi == 0) {
+                break;
+            }
 
-            Graf graf = new Graf(jumlahKota);
-
+            List<List<Integer>> daftarAliansi = new ArrayList<>();
             for (int i = 0; i < jumlahAliansi; i++) {
                 int kotaDalamAliansi = scanner.nextInt();
-                List<Integer> kotaKota = new ArrayList<>();
+                List<Integer> aliansi = new ArrayList<>();
                 for (int j = 0; j < kotaDalamAliansi; j++) {
-                    kotaKota.add(scanner.nextInt());
+                    aliansi.add(scanner.nextInt());
                 }
-                graf.tambahAliansi(i, kotaKota);
+                daftarAliansi.add(aliansi);
             }
 
-            int kotaMulai = -1;
-            for (int i = 0; i < jumlahKota; i++) {
-                if (graf.bisaKunjungiSemuaKotaDari(i)) {
-                    kotaMulai = i;
-                    break;
-                }
-            }
-
-            output.append(kotaMulai).append("\n");
+            int kotaAwal = cariKotaAwal(jumlahKota, jumlahAliansi, daftarAliansi);
+            hasil.add(kotaAwal);
         }
 
-        System.out.print(output);
+        for (int kota : hasil) {
+            System.out.println(kota);
+        }
         scanner.close();
     }
-}
